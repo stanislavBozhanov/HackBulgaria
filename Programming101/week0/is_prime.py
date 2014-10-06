8a5544230697676bf4a4c551a6fc8b88b1fd8dac
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2014-10-06 20:06:37 +0300

    two more tasks

---
+++ Programming101/week0/is_prime.py
@@ -0,0 +1,20 @@
+def is_prime(n):
+    prime = True
+    if n < 2:
+        return False
+    for number in range(2, n-1):
+        if n % number == 0:
+            prime = False
+    return prime
+
+def main():
+    print(is_prime(1))
+    print(is_prime(2))
+    print(is_prime(8))
+    print(is_prime(11))
+    print(is_prime(-10))
+    print(is_prime(97))
+    print(is_prime(101))
+
+if __name__ == '__main__':
+    main()
---
+++ Programming101/week0/prime_number_of_divisors.py
@@ -0,0 +1,21 @@
+from is_prime import is_prime
+
+def number_of_divisors(n):
+    count = 0
+    for number in range(1, n+1):
+        if n % number == 0:
+            count += 1
+    return count
+
+
+def prime_number_of_divisors(n):
+    return is_prime(number_of_divisors(n))
+
+
+def main():
+    print(prime_number_of_divisors(7))
+    print(prime_number_of_divisors(8))
+    print(prime_number_of_divisors(9))
+
+if __name__ == '__main__':
+    main()
