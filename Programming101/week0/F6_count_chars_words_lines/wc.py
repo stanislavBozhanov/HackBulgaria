95fbed592c82a4fde4f35ceb88cdde2a60f2173e
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2014-10-12 12:02:20 +0300

    F6-returns correct chars but wrong lines and words

---
+++ Programming101/week0/F6_count_chars_words_lines/wc.py
@@ -0,0 +1,26 @@
+import sys
+
+
+def wc(command, filename):
+    txt_file = open(filename, 'r')
+    content = txt_file.read()
+    if command == 'chars':
+        return len(content)
+    elif command == 'words':
+        words = content.split(' ')
+        return len(words)
+    elif command == 'lines':
+        lines = content.splitlines()
+        return len(lines)
+    else:
+        return 'Wrong command!'
+
+
+def main():
+    command = sys.argv[1]
+    filename = sys.argv[2]
+    print(wc(command, filename))
+
+
+if __name__ == '__main__':
+    main()
