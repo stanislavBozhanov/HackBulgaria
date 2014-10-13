525d00a1788e9afa75395a141f3f27c4d67434a3
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2014-10-14 00:05:59 +0300

    Fixed coordinates

--- Programming101/week0/matrix_bombing_plan.py
+++ Programming101/week0/matrix_bombing_plan.py
@@ -1,24 +1,19 @@
 def init_battlefiled(size):
     result_dict = {}
-    for x in range(size[0]):
-        for y in range(size[1]):
-            result_dict[(x, y)] = 0
+    for r in range(size[0]):
+        for c in range(size[1]):
+            result_dict[(r, c)] = 0
     return result_dict
 
 
-def print_result(m_dict):
-    for field in m_dict:
-        print(field, ":", m_dict[field])
-
-
 def get_size(m):
-    x, y = len(m[0]), len(m)
-    size = (x, y)
+    r, c = len(m), len(m[0])
+    size = (r, c)
     return size
 
 
-def fileds_exist(xy, sz):
-    if xy[0] >= 0 and xy[0] <= sz[0] - 1 and xy[1] >= 0 and xy[1] <= sz[1] - 1:
+def fileds_exist(rc, sz):
+    if rc[0] >= 0 and rc[0] <= sz[0] - 1 and rc[1] >= 0 and rc[1] <= sz[1] - 1:
         return True
     return False
 
@@ -38,25 +33,30 @@ def bomb(m, field):
                (-1, 1), (0, 1), (1, 1)]
     for item in temp_xy:
         cur_field = (field[0] + item[0], field[1] + item[1])
-        y1 = cur_field[0]
-        x1 = cur_field[1]
-        y2 = field[0]
-        x2 = field[1]
+        r1 = cur_field[0]
+        c1 = cur_field[1]
+        r2 = field[0]
+        c2 = field[1]
         if fileds_exist(cur_field, size):
-            temp_m[x1][y1] = temp_m[x1][y1] - temp_m[x2][y2]
-            if temp_m[x1][y1] < 0:
-                temp_m[x1][y1] = 0
+            temp_m[r1][c1] = temp_m[r1][c1] - temp_m[r2][c2]
+            if temp_m[r1][c1] < 0:
+                temp_m[r1][c1] = 0
     return temp_m
 
 
 def matrix_bombing_plan(m):
     size = get_size(m)
     plan = init_battlefiled(size)
-    for field in plan:
-        temp_m = bomb(m, field)
-        total_sum = sum_matrix(temp_m)
-        plan[field] = total_sum
+    for r in range(size[0]):
+        for c in range(size[1]):
+            field = (r, c)
+            temp_m = bomb(m, field)
+            print(temp_m)
+            total_sum = sum_matrix(temp_m)
+            plan[field] = total_sum
+            print(field, ":", plan[field])
 
 
 if __name__ == '__main__':
     print(matrix_bombing_plan([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+     
