109048dd5c0c1999346097653a5af559e1d06081
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2014-10-15 19:42:50 +0300

    Changes to cash_desk

--- Programming101/week1/1-Python-OOP-problems-set/cash-desk.py
+++ Programming101/week1/1-Python-OOP-problems-set/cash-desk.py
@@ -7,18 +7,17 @@ class CashDesk(object):
         self.inserted = inserted
         for note, amount in inserted.items():
             self.money[note] += amount
-"""
+
     def total(self):
         total_sum = 0
         for note, amount in self.money.items():
-            total_sum += int(self.money[amount]) * int(note)
+            total_sum += int(amount) * int(note)
         return total_sum
-
+"""
     def can_withdraw_money(self, amount_of_money):
         self.amount_of_money = amount_of_money
         for note, amount in  amount_of_money.items():
 """
 my_cash_desk = CashDesk()
 my_cash_desk.take_money({1: 2, 50: 1, 20: 1})
-print(my_cash_desk.money)
-# my_cash_desk.total()
+print(my_cash_desk.total())
