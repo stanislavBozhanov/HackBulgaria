5b09d4dfe3c7a1df8a2f48b93ea664402fa79cc6
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2014-10-17 20:37:53 +0300

    removed lines

--- Programming101/week0/python_harder_problem_set/fibonacci_lists/fibonacci_lists.py
+++ Programming101/week0/python_harder_problem_set/fibonacci_lists/fibonacci_lists.py
@@ -11,15 +11,3 @@ def nth_fib_lists(listA, listB, n):
             listA = listB
             listB = temp
         return listB
-
-
-def main():
-    print(nth_fib_lists([1], [2], 2))
-    print(nth_fib_lists([1], [2], 2))
-    print(nth_fib_lists([1, 2], [1, 3], 3))
-    print(nth_fib_lists([], [1, 2, 3], 6))
-    print(nth_fib_lists([0], [0], 100))
-
-
-if __name__ == '__main__':
-    main()
