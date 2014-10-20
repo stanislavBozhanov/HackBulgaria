6bb5dd30c1c185a411b50a231709f2e979fe1256
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2014-10-20 20:05:39 +0300

    hero added healing and take damage

--- Programming101/week2/hero.py
+++ Programming101/week2/hero.py
@@ -3,6 +3,28 @@ class Hero(object):
         self.name = name
         self.health = health
         self.nickname = nickname
+        self._MAX_HEALTH = health
 
     def known_as(self):
         return "{} the {}".format(self.name, self.nickname)
+
+    def get_health(self):
+        return self.health
+
+    def is_alive(self):
+        return self.health > 0
+
+    def take_damage(self, damage):
+        self.health -= damage
+        if self.health < 0:
+            self.health = 0
+
+    def take_healing(self, healing):
+        if self.health == 0:
+            return False
+        else:
+            if self._MAX_HEALTH < self.health + healing:
+                self.health = self._MAX_HEALTH
+            else:
+                self.health += healing
+        return True
--- Programming101/week2/hero_tests.py
+++ Programming101/week2/hero_tests.py
@@ -15,7 +15,34 @@ class TestHero(unittest.TestCase):
         self.assertEqual(self.sten.known_as(), "Bron the DragonSlayer")
 
     def test_get_health(self):
-        pass
+        self.assertEqual(self.sten.get_health(), 100)
+
+    def test_is_alive(self):
+        self.assertTrue(self.sten.is_alive())
+
+    def test_not_alive(self):
+        self.sten.health = 0
+        self.assertFalse(self.sten.is_alive())
+
+    def test_take_damage(self):
+        self.sten.take_damage(50)
+        self.assertEqual(self.sten.health, 50)
+        self.sten.take_damage(60)
+        self.assertEqual(self.sten.health, 0)
+
+    def test_take_healing(self):
+        self.sten.health = 50
+        self.assertTrue(self.sten.take_healing(20))
+        self.assertEqual(self.sten.health, 70)
+
+    def test_take_healing_above_max(self):
+        self.sten.health = 20
+        self.sten.take_healing(120)
+        self.assertEqual(self.sten.health, 100)
+
+    def test_healing_on_dead(self):
+        self.sten.health = 0
+        self.assertFalse(self.sten.take_healing(100))
 
 if __name__ == '__main__':
     unittest.main()
