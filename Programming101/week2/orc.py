528646408c45de24ccaab77534cf30f77de4bcce
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2014-10-20 21:34:02 +0300

    validate berserk factor

--- Programming101/week2/orc.py
+++ Programming101/week2/orc.py
@@ -4,4 +4,10 @@ from entity import Entity
 class Orc(Entity):
     def __init__(self, name, health, berserk_factor):
         super().__init__(name, health)
-        self.berserk_factor = berserk_factor
+        self._set_berserk_factor(berserk_factor)
+
+    def _set_berserk_factor(self, berserk_factor):
+        if berserk_factor > 1 and berserk_factor < 2:
+            self.berserk_factor = berserk_factor
+        else:
+            raise ValueError
--- Programming101/week2/orc_tests.py
+++ Programming101/week2/orc_tests.py
@@ -9,5 +9,10 @@ class TestOrc(unittest.TestCase):
     def test_init(self):
         self.assertEqual(self.sho.berserk_factor, 1.34353)
 
+    def test_init_incorrect_berserk(self):
+        with self.assertRaises(ValueError):
+            Orc("Vilasteros2", 400, 2.32434)
+
+
 if __name__ == '__main__':
     unittest.main()
