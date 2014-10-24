676508a56df0fa5a30f01352f3b6610ed0d313e1
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2014-10-24 21:41:00 +0300

    added new methods and test for them

--- Programming101/week2/MusicLibrary/playlist.py
+++ Programming101/week2/MusicLibrary/playlist.py
@@ -3,6 +3,14 @@ class Playlist():
         self.name = name
         self.songs = []
 
+    def __str__(self):
+        output = ""
+        for item in self.songs:
+            minutes = item.length // 60
+            seconds = item.length % 60
+            output += "{} {} - {:02d}:{:02d}\n".format(item.artist, item.name, minutes, seconds)
+        return output
+
     def add_song(self, song):
         self.songs.append(song)
 
@@ -18,3 +26,24 @@ class Playlist():
         for item in self.songs:
             seconds += item.length
         return seconds
+
+    def remove_disrated(self, rating):
+        index = len(self.songs) - 1
+        while index >= 0:
+            if self.songs[index].rating < rating:
+                self.songs[index] = None
+            index = index - 1
+
+    def remove_bad_quality(self):
+        index = len(self.songs) - 1
+        while index >= 0:
+            if self.songs[index].bitrate < 128:
+                self.songs[index] = None
+            index = index - 1
+
+    def show_artists(self):
+        artists = []
+        for item in self.songs:
+            artists.append(item.artist)
+        artists = list(set(artists))
+        return artists
--- Programming101/week2/MusicLibrary/playlist_test.py
+++ Programming101/week2/MusicLibrary/playlist_test.py
@@ -15,8 +15,18 @@ class TestPlaylist(unittest.TestCase):
             320
 
         )
+        self.rubyDNB = Song(
+            "Ruby DNB",
+            "Kaiser Chiefs",
+            "Unknown",
+            4,
+            300,
+            120
+
+        )
         self.plist.add_song(self.ruby)
         self.plist.add_song(self.ruby)
+        self.plist.add_song(self.rubyDNB)
 
     def test_init(self):
         self.assertEqual(self.plist.name, "plist out")
@@ -29,7 +39,26 @@ class TestPlaylist(unittest.TestCase):
         self.assertNotIn(self.ruby, self.plist.songs)
 
     def test_total_length(self):
-        self.assertEqual(self.plist.total_length(), 600)
+        self.assertEqual(self.plist.total_length(), 900)
+
+    def test_rating_out_of_range(self):
+        for item in self.plist.songs:
+            self.assertIn(item.rating, [1, 2, 3, 4, 5])
+
+    def test_remove_disrated(self):
+        self.plist.remove_disrated(3)
+        self.assertIn(self.rubyDNB, self.plist.songs)
+        self.assertNotIn(self.ruby, self.plist.songs)
+
+    def test_remove_bad_quality(self):
+        self.plist.remove_bad_quality()
+        self.assertNotIn(self.rubyDNB, self.plist.songs)
+
+    def test_show_artists(self):
+        self.assertEqual(self.plist.show_artists(), ["Kaiser Chiefs"])
+
+    def test_print_list(self):
+        print(self.plist)
 
 if __name__ == '__main__':
     unittest.main()
