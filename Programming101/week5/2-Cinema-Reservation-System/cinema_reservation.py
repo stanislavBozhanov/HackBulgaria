deada06bfa5c4ba6d292eadd3c01b1fe28ac464d
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2014-11-12 20:18:56 +0200

    methods insert_movie and show_movies

--- Programming101/week5/2-Cinema-Reservation-System/cinema_reservation.py
+++ Programming101/week5/2-Cinema-Reservation-System/cinema_reservation.py
@@ -2,11 +2,11 @@ import sqlite3
 
 
 def create_table_movies():
-    db = sqlite3.connect('movies.db')
+    db = sqlite3.connect('cinema.db')
     cursor = db.cursor()
-
     cursor.execute('''CREATE TABLE IF NOT EXISTS movies(
-                id INTEGER  PRIMARY KEY, name TEXT,
+                id INTEGER  PRIMARY KEY,
+                name TEXT,
                 rating REAL
                 )''')
     db.commit()
@@ -14,9 +14,8 @@ def create_table_movies():
 
 
 def create_table_projections():
-    db = sqlite3.connect('projections.db')
+    db = sqlite3.connect('cinema.db')
     cursor = db.cursor()
-
     cursor.execute('''CREATE TABLE IF NOT EXISTS projections(
                 id INTEGER PRIMARY KEY,
                 movie_id INTEGER,
@@ -30,9 +29,8 @@ def create_table_projections():
 
 
 def create_table_reservations():
-    db = sqlite3.connect('reservations.db')
+    db = sqlite3.connect('cinema.db')
     cursor = db.cursor()
-
     cursor.execute('''CREATE TABLE IF NOT EXISTS reservations(
                 id INTEGER  PRIMARY KEY,
                 username TEXT,
@@ -45,8 +43,38 @@ def create_table_reservations():
     db.close()
 
 
+def insert_movie():
+    name = input('movie_name>')
+    rating = float(input('movie_rating>'))
+    db = sqlite3.connect('cinema.db')
+    cursor = db.cursor()
+    cursor.execute('''INSERT INTO movies(name, rating) VALUES(?,?)''', (name, rating))
+    db.commit()
+    db.close()
+
+
+def show_movies():
+    connection = sqlite3.connect('cinema.db')
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+    result = cursor.execute('SELECT id, name, rating FROM movies')
+    for row in result:
+        print('[{id}] - {name} ({rating})'.format(**row))
+
+
 def main():
-    pass
+    create_table_movies()
+    create_table_projections()
+    create_table_reservations()
+    while True:
+        command = input('command>')
+        command = command.split()
+        if command[0] == 'show_movies':
+            show_movies()
+        elif command[0] == 'insert_movie':
+            insert_movie()
+        elif command[0] == 'break':
+            break
 
 if __name__ == '__main__':
     main()
