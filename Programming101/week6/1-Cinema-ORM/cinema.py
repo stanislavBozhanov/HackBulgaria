05cafe9278a709a49679319766dbe99a62f38c61
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2014-11-24 14:45:23 +0200

    moved methods in class cinema

--- Programming101/week6/1-Cinema-ORM/cinema.py
+++ Programming101/week6/1-Cinema-ORM/cinema.py
@@ -1,9 +1,44 @@
 from movie import Movie
-# from projections import Projections
-# from reservations import Reservations
-# from sqlalchemy import create_engine
+from projection import Projection
+from reservation import Reservation
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 
 class Cinema:
     def __init__(self):
-        pass
+        self.__engine = create_engine('sqlite:///cinema.db')
+        self.__session = Session(bind=self.__engine)
+
+    def show_movies(self):
+        all_movies = self.__session.query(Movie).order_by(Movie.rating)
+        for movie in all_movies:
+            print(movie)
+
+    def add_movie(self):
+        name = input('name>')
+        rating = float(input('rating>'))
+        new_movie = Movie(name=name, rating=rating)
+        self.__session.add(new_movie)
+        self.__session.commit()
+
+    def add_projection(self):
+        type_ = input('type>')
+        date_ = input('date>')
+        time_ = input('time>')
+        movie_id = int(input('movie_id'))
+        new_projection = Projection(type_=type_, date_=date_, time_=time_, movie_id=movie_id)
+        self.__session.add(new_projection)
+        self.__session.commit()
+
+    def add_reservation(self):
+        un = input('username>')
+        row = int(input('row>'))
+        col = int(input('col>'))
+        p_id = int(input('projection_id>'))
+        new_reservation = Reservation(username=un, row=row, col=col, projection_id=p_id)
+        self.__session.add(new_reservation)
+        self.__session.commit()
+
+    # def show_movie_projection():
+    #     m_id = int(input('movie_id>'))
--- Programming101/week6/1-Cinema-ORM/main.py
+++ Programming101/week6/1-Cinema-ORM/main.py
@@ -1,10 +1,10 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 from connection import Base
-from movie import Movie
-from projection import Projection
-from reservation import Reservation
-# from cinema import Cinema
+from cinema import Cinema
+# from movie import Movie
+# from projection import Projection
+# from reservation import Reservation
 
 
 engine = create_engine('sqlite:///cinema.db')
@@ -31,53 +31,20 @@ def reserve_seats(reserved):
     return matrix
 
 
-def show_movies():
-    all_movies = session.query(Movie).order_by(Movie.rating)
-    for movie in all_movies:
-        print(movie)
-
-
-def add_movie():
-    name = input('name>')
-    rating = float(input('rating>'))
-    new_movie = Movie(name=name, rating=rating)
-    session.add(new_movie)
-    session.commit()
-
-
-def add_projection():
-    type_ = input('type>')
-    date_ = input('date>')
-    time_ = input('time>')
-    movie_id = int(input('movie_id'))
-    new_projection = Projection(type_=type_, date_=date_, time_=time_, movie_id=movie_id)
-    session.add(new_projection)
-    session.commit()
-
-
-def add_reservation():
-    un = input('username>')
-    row = int(input('row>'))
-    col = int(input('col>'))
-    p_id = int(input('projection_id>'))
-    new_reservation = Reservation(username=un, row=row, col=col, projection_id=p_id)
-    session.add(new_reservation)
-    session.commit()
-
-
 def main():
     Base.metadata.create_all(engine)
+    arena = Cinema()
 
     while True:
         command = input(">>>")
         if command == 'add_movie':
-            add_movie()
+            arena.add_movie()
         elif command == 'add_projection':
-            add_projection()
+            arena.add_projection()
         elif command == 'add_reservation':
-            add_reservation()
+            arena.add_reservation()
         elif command == 'show_movies':
-            show_movies()
+            arena.show_movies()
 
 if __name__ == '__main__':
     main()
