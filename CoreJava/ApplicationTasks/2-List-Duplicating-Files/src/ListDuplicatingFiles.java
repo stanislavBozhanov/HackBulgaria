90dfd45c59390ff07e01151645c88fe761249d21
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2015-02-18 12:29:47 +0200

    Refactored into two methods

--- CoreJava/ApplicationTasks/2-List-Duplicating-Files/src/ListDuplicatingFiles.java
+++ CoreJava/ApplicationTasks/2-List-Duplicating-Files/src/ListDuplicatingFiles.java
@@ -1,39 +1,50 @@
 import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
 public class ListDuplicatingFiles {
-    public static void listDuplicatingFiles(File folder, List<File> result) throws IOException {
+    public static void getAllFiles(File folder, List<File> result) {
         File[] listOfFiles = folder.listFiles();
         for (int i = 0; i < listOfFiles.length; i++) {
             if (listOfFiles[i].isFile()) {
-                boolean duplicate = false;
-                byte[] f1 = Files.readAllBytes(listOfFiles[i].toPath());
-                for (File file : result) {
-                    byte[] f2 = Files.readAllBytes(file.toPath());
-                    if (Arrays.equals(f1, f2))
-                    {
-                        duplicate = true;
-                    }
-                }
-                if (!duplicate) {
-                    result.add(listOfFiles[i]);
-                }
+               result.add(listOfFiles[i]);
             }
             if (listOfFiles[i].isDirectory()) {
-                listDuplicatingFiles(listOfFiles[i], result);
+                getAllFiles(listOfFiles[i], result);
+            }
+        }
+    }
+
+    public static void getUniqueFiles(List<File> allFiles, List<File> uniqueFiles) throws IOException {
+        for (int i = 0; i < allFiles.size(); i++) {
+            boolean duplicate = false;
+            byte[] f1 = Files.readAllBytes(allFiles.get(i).toPath());
+            for (File file : uniqueFiles) {
+                byte[] f2 = Files.readAllBytes(file.toPath());
+                if (Arrays.equals(f1, f2))
+                {
+                    duplicate = true;
+                }
+            }
+            if (!duplicate) {
+                uniqueFiles.add(allFiles.get(i));
             }
         }
     }
 
     public static void main(String[] args) throws Exception {
-        File startingPoint = new File("/home/stenly/TempStuff/TestDirectories/Test1");
-        List<File> result = new ArrayList<File>();
-        listDuplicatingFiles(startingPoint, result);
-        for (File file : result) {
+        File startingPoint = new File("/home/stenly/TempStuff/TestDirectories");
+        List<File> allFiles = new ArrayList<File>();
+        getAllFiles(startingPoint, allFiles);
+
+        List<File> uniqueFiles = new ArrayList<File>();
+        getUniqueFiles(allFiles, uniqueFiles);
+        for (File file : uniqueFiles) {
             System.out.println(file);
         }
     }
